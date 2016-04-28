60981369e5c34906a298a89be83266daa5b5e195
DMRodrigues <diogo.miguel.rodrigues@ist.utl.pt>
2016-04-28 11:22:01 +0100

    login and register added

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/LoginActivity.java
@@ -0,0 +1,39 @@
+package pt.ulisboa.tecnico.cmov.ubibike;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button bLogin = (Button) findViewById(R.id.buttonLogin);
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
+
+        Button bRegister = (Button) findViewById(R.id.buttonRegister);
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+    }
+
+}
