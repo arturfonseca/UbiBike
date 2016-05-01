3031aba56a3470dd9f1956d57fddb0c172a70d59
arturfonseca <artur.fonseca@live.com>
2016-05-01 17:47:22 +0100

    login+regist

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/domain/HtmlConnections.java
@@ -0,0 +1,35 @@
+package pt.ulisboa.tecnico.cmov.ubibike.domain;
+
+import android.os.AsyncTask;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * Created by Artur Fonseca on 28/04/2016.
+ */
+public final class HtmlConnections {
+
+
+   public static String getResponse(String cmd) {
+        String stringText = "";
+        String url = "http://10.0.2.2:8000/run?";
+        try {
+            URL textUrl = new URL(url + cmd);
+            BufferedReader bufferReader = new BufferedReader(new InputStreamReader(textUrl.openStream()));
+            String StringBuffer;
+
+            while ((StringBuffer = bufferReader.readLine()) != null) {
+                stringText += StringBuffer;
+            }
+            bufferReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            stringText = "error";
+        }
+        return stringText;
+    }
+}
+
