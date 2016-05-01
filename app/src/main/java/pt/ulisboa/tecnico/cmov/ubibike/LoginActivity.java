3f18deab99d1ff81165a7f33fbc1823e55147587
arturfonseca <artur.fonseca@live.com>
2016-05-01 17:46:06 +0100

    login+regist

--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/LoginActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/LoginActivity.java
@@ -1,13 +1,15 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import pt.ulisboa.tecnico.cmov.ubibike.domain.HtmlConnections;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,7 +24,20 @@ public class LoginActivity extends AppCompatActivity {
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                try {
+                    EditText text = (EditText) findViewById(R.id.ExtractUsername);
+                    String name = text.getText().toString();
+                    text = (EditText) findViewById(R.id.ExtractPassword);
+                    String pw = text.getText().toString();
+                    if (name.equals(".") && pw.equals("."))  //FOR TEST ONLY
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    else
+                        new GetResult().execute("login:" + name + "," + pw);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
             }
         });
 
@@ -34,6 +49,24 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    private class GetResult extends AsyncTask<String, String, String> {
+        String output;
+
+        protected String doInBackground(String... url) {
+            output = HtmlConnections.getResponse(url[0]);
+            return null;
+        }
+
+        protected void onPostExecute(String result) {
+            if (output.equals("OK"))
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            else
+                System.out.println("Error");
+
+        }
     }
 
 }
--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/RegisterActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/RegisterActivity.java
@@ -1,11 +1,17 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import pt.ulisboa.tecnico.cmov.ubibike.domain.HtmlConnections;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,16 +22,43 @@ public class RegisterActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+       Button bRegist = (Button) findViewById(R.id.buttonRegister);
+       bRegist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                try {
+                    EditText text = (EditText) findViewById(R.id.editTextUsername);
+                    String name = text.getText().toString();
+                    text = (EditText) findViewById(R.id.editTextPassword);
+                    String pw1 = text.getText().toString();
+                    text = (EditText) findViewById(R.id.editTextRetypePassword);
+                    String pw2 = text.getText().toString();
+                    if (pw1.equals(pw2))
+                        new GetResult().execute("regist:" + name + "," + pw1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
-        */
+
+    }
+
+    private class GetResult extends AsyncTask<String, String, String> {
+        String output;
+
+        protected String doInBackground(String... url) {
+            output = HtmlConnections.getResponse(url[0]);
+            return null;
+        }
+
+        protected void onPostExecute(String result) {
+            if (output.equals("OK"))
+                startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+            else
+                System.out.println("Error");
+
+        }
     }
 
 }
